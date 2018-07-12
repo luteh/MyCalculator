a1d7dac5aef26f5459e97b525b0ff44f7536e75f
luteh <luthfanmaftuh@gmail.com>
2018-07-12 13:46:56 +0700

    4th commit

--- app/src/main/java/com/luteh/mycalculator/MainActivity.java
+++ app/src/main/java/com/luteh/mycalculator/MainActivity.java
@@ -3,12 +3,99 @@ package com.luteh.mycalculator;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private enum OPERATOR {
+        PLUS, SUBTRACT, MULTIPLY, DIVIDE, EQUAL
+    }
+
+    TextView txtCalculation, txtResults;
+
+    // Instance Variables
+
+    private String currentNumber, stringNumberAtLeft, stringNumberAtRight;
+    private OPERATOR currentOperator;
+    private int calculationsResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currentNumber = "";
+        calculationsResult = 0;
+
+        txtCalculation = findViewById(R.id.txtCalculation);
+        txtResults = findViewById(R.id.txtResults);
+
+        findViewById(R.id.btn0).setOnClickListener(MainActivity.this);
+        findViewById(R.id.btn1).setOnClickListener(MainActivity.this);
+        findViewById(R.id.btn2).setOnClickListener(MainActivity.this);
+        findViewById(R.id.btn3).setOnClickListener(MainActivity.this);
+        findViewById(R.id.btn4).setOnClickListener(MainActivity.this);
+        findViewById(R.id.btn5).setOnClickListener(MainActivity.this);
+        findViewById(R.id.btn6).setOnClickListener(MainActivity.this);
+        findViewById(R.id.btn7).setOnClickListener(MainActivity.this);
+        findViewById(R.id.btn8).setOnClickListener(MainActivity.this);
+        findViewById(R.id.btn9).setOnClickListener(MainActivity.this);
+        findViewById(R.id.btnPlus).setOnClickListener(MainActivity.this);
+        findViewById(R.id.btnMultiply).setOnClickListener(MainActivity.this);
+        findViewById(R.id.btnSubtract).setOnClickListener(MainActivity.this);
+        findViewById(R.id.btnDivide).setOnClickListener(MainActivity.this);
+        findViewById(R.id.btnEqual).setOnClickListener(MainActivity.this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn0:
+                numberIsTapped(0);
+                break;
+            case R.id.btn1:
+                numberIsTapped(1);
+                break;
+            case R.id.btn2:
+                numberIsTapped(2);
+                break;
+            case R.id.btn3:
+                numberIsTapped(3);
+                break;
+            case R.id.btn4:
+                numberIsTapped(4);
+                break;
+            case R.id.btn5:
+                numberIsTapped(5);
+                break;
+            case R.id.btn6:
+                numberIsTapped(6);
+                break;
+            case R.id.btn7:
+                numberIsTapped(7);
+                break;
+            case R.id.btn8:
+                numberIsTapped(8);
+                break;
+            case R.id.btn9:
+                numberIsTapped(9);
+                break;
+            case R.id.btnPlus:
+                break;
+            case R.id.btnSubtract:
+                break;
+            case R.id.btnMultiply:
+                break;
+            case R.id.btnDivide:
+                break;
+            case R.id.btnEqual:
+                break;
+        }
+    }
+
+    private void numberIsTapped(int tappedNumber){
+        currentNumber = currentNumber + String.valueOf(tappedNumber);
+        txtResults.setText(currentNumber);
     }
 }
